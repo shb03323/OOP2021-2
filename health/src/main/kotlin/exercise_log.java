787af99cadbbfd78b1f1e0ae8ac6fbb8d4e6f65f
mihnhyuk <mihnhyuk@naver.com>
2021-12-02 23:07:26 +0900

    1202

---
+++ health/src/main/kotlin/exercise_log.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+import java.util.HashMap;
+
+public class exercise_log {
+  private HashMap<LocalDate, exercise> record = new HashMap<LocalDate, exercise>();
+
+  public
+}
